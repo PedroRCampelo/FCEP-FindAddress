b1cce65724257da9e3126b053ce186c2be3cf395
PedroRCampelo <pedrocampelo@live.com>
2022-11-20 09:01:00 -0300

    Creating validations and events on click

--- src/cep/About.java
+++ src/cep/About.java
@@ -1,11 +1,16 @@
 package cep;
 
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Desktop;
 import java.awt.EventQueue;
+import java.awt.Toolkit;
+import java.net.URI;
 
 import javax.swing.JDialog;
-import java.awt.Toolkit;
 import javax.swing.JLabel;
-import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class About extends JDialog {
 
@@ -30,29 +35,48 @@ public class About extends JDialog {
 	 * Create the dialog.
 	 */
 	public About() {
+		setModal(true);
 		setTitle("Sobre");
 		setIconImage(Toolkit.getDefaultToolkit().getImage(About.class.getResource("/img/home.png")));
 		setResizable(false);
-		setBounds(100, 100, 450, 300);
+		setBounds(150, 150, 450, 300);
 		getContentPane().setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("Find Address --V01");
 		lblNewLabel.setBounds(28, 27, 165, 16);
 		getContentPane().add(lblNewLabel);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("@Autor Pedro Campelo");
 		lblNewLabel_1.setBounds(28, 73, 215, 16);
 		getContentPane().add(lblNewLabel_1);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("Web Service:");
 		lblNewLabel_2.setBounds(28, 222, 88, 16);
 		getContentPane().add(lblNewLabel_2);
-		
-		JLabel lblNewLabel_3 = new JLabel("republicavirtual.com.br");
-		lblNewLabel_3.setForeground(Color.BLUE);
-		lblNewLabel_3.setBounds(117, 222, 157, 16);
-		getContentPane().add(lblNewLabel_3);
 
+		JLabel lblWebService = new JLabel("republicavirtual.com.br");
+		lblWebService.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				link("https://www.republicavirtual.com.br/"); // !
+			}
+		});
+		lblWebService.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		lblWebService.setForeground(Color.BLUE);
+		lblWebService.setBounds(117, 222, 157, 16);
+		getContentPane().add(lblWebService);
+
+	} // End of constructor
+
+	// Link Method
+	private void link(String website) {
+		Desktop desktop = Desktop.getDesktop();
+		try {
+			URI uri = new URI(website);
+			desktop.browse(uri);
+		} catch (Exception e) {
+			System.out.println(e); 
+		}
 	}
 
 }
--- src/cep/Cep.java
+++ src/cep/Cep.java
@@ -1,26 +1,32 @@
 package cep;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JComboBox;
-import javax.swing.JButton;
-import javax.swing.ImageIcon;
-import java.awt.SystemColor;
 import java.awt.Cursor;
+import java.awt.EventQueue;
+import java.awt.SystemColor;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import Atxy2k.CustomTextField.RestrictedTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 @SuppressWarnings("serial")
 public class Cep extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
+	private JTextField txtCep;
+	private JTextField txtAddress;
+	private JTextField txtDistrict;
+	private JTextField txtCity;
 
 	/**
 	 * Launch the application.
@@ -44,72 +50,107 @@ public class Cep extends JFrame {
 	public Cep() {
 		setResizable(false);
 		setTitle("Buscar endereço");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);     
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("CEP");
 		lblNewLabel.setBounds(26, 33, 29, 16);
 		contentPane.add(lblNewLabel);
-		
-		textField = new JTextField();
-		textField.setBounds(55, 28, 130, 26);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
+
+		txtCep = new JTextField();
+		txtCep.setBounds(55, 28, 130, 26);
+		contentPane.add(txtCep);
+		txtCep.setColumns(10);
+
 		JLabel lblNewLabel_1 = new JLabel("Endereço");
 		lblNewLabel_1.setBounds(42, 96, 61, 16);
 		contentPane.add(lblNewLabel_1);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("Bairro");
 		lblNewLabel_2.setBounds(42, 141, 61, 16);
 		contentPane.add(lblNewLabel_2);
-		
+
 		JLabel lblNewLabel_3 = new JLabel("Cidade");
 		lblNewLabel_3.setBounds(42, 189, 61, 16);
 		contentPane.add(lblNewLabel_3);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(106, 91, 210, 26);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
-		
-		textField_2 = new JTextField();
-		textField_2.setBounds(86, 136, 151, 26);
-		contentPane.add(textField_2);
-		textField_2.setColumns(10);
-		
-		textField_3 = new JTextField();
-		textField_3.setBounds(96, 184, 141, 26);
-		contentPane.add(textField_3);
-		textField_3.setColumns(10);
-		
+
+		txtAddress = new JTextField();
+		txtAddress.setBounds(106, 91, 210, 26);
+		contentPane.add(txtAddress);
+		txtAddress.setColumns(10);
+
+		txtDistrict = new JTextField();
+		txtDistrict.setBounds(86, 136, 151, 26);
+		contentPane.add(txtDistrict);
+		txtDistrict.setColumns(10);
+
+		txtCity = new JTextField();
+		txtCity.setBounds(96, 184, 141, 26);
+		contentPane.add(txtCity);
+		txtCity.setColumns(10);
+
 		JLabel lblNewLabel_4 = new JLabel("UF");
 		lblNewLabel_4.setBounds(276, 189, 23, 16);
 		contentPane.add(lblNewLabel_4);
-		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(303, 185, 73, 27);
-		contentPane.add(comboBox);
-		
-		JButton btnNewButton = new JButton("Buscar");
-		btnNewButton.setBounds(197, 28, 94, 29);
-		contentPane.add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("Limpar");
-		btnNewButton_1.setBounds(26, 226, 109, 29);
-		contentPane.add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("Sobre");
-		btnNewButton_2.setBorder(null);
-		btnNewButton_2.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnNewButton_2.setBackground(SystemColor.control);
-		btnNewButton_2.setIcon(new ImageIcon(Cep.class.getResource("/img/about.png")));
-		btnNewButton_2.setBounds(327, 17, 117, 51);
-		contentPane.add(btnNewButton_2);
-	}
+
+		JComboBox cboUf = new JComboBox();
+		cboUf.setModel(new DefaultComboBoxModel(
+				new String[] { "", "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA",
+						"PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO" }));
+		cboUf.setBounds(303, 185, 73, 27);
+		contentPane.add(cboUf);
+
+		// Click event (Button buscar)
+		JButton btnCep = new JButton("Buscar");
+		btnCep.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnCep.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (txtCep.getText().equals("")) {
+					JOptionPane.showMessageDialog(null, "Informe o CEP");
+					txtCep.requestFocus();
+				} else if (txtCep.getText().length() < 8) {
+					JOptionPane.showMessageDialog(null, "Cep incompleto");
+					txtCep.requestFocus();
+				} else {
+					// Find address
+				}
+			}
+		});
+
+		btnCep.setBounds(197, 28, 94, 29);
+		contentPane.add(btnCep);
+
+		JButton btnClear = new JButton("Limpar");
+		btnClear.setBounds(26, 226, 109, 29);
+		contentPane.add(btnClear);
+
+		// Click event (Button Sobre)
+		JButton btnAbout = new JButton("Sobre");
+		btnAbout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				About about = new About(); 
+				about.setVisible(true);
+			}
+		});
+		btnAbout.setBorder(null);
+		btnAbout.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnAbout.setBackground(SystemColor.control);
+		btnAbout.setIcon(new ImageIcon(Cep.class.getResource("/img/about.png")));
+		btnAbout.setBounds(327, 17, 117, 51);
+		contentPane.add(btnAbout);
+
+		// Validation fields
+		// Command + Shift + o TO IMPORT the class
+
+		// ATXY2K
+		RestrictedTextField valid = new RestrictedTextField(txtCep);
+		valid.setOnlyNums(true);
+		valid.setLimit(8);
+
+	} // End Constructor
 }
